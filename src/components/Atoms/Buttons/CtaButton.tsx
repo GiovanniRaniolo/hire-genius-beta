320d26f7544accd430e9ade572b56fc41543a145
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-10-02 14:50:38 +0200

    style: update colors and various on storic profile

--- src/components/Atoms/Buttons/CtaButton.tsx
+++ src/components/Atoms/Buttons/CtaButton.tsx
@@ -1,23 +1,24 @@
+import React from "react";
 import style from "./Button.module.scss";
 
 interface ButtonProps {
-	label: string;
-	className: string;
-	onClick?: () => void;
-	type?: "submit" | "reset" | "button" | undefined;
-	disabled?: boolean;
+  label: string;
+  className: string;
+  onClick?: () => void;
+  type?: "submit" | "reset" | "button" | undefined;
+  disabled?: boolean;
 }
 
 const CtaButton = (props: ButtonProps) => {
-	const { label, className, onClick, type, disabled = false } = props;
+  const { label, className, onClick, type, disabled = false } = props;
 
-	return (
-		<>
-			<button className={`${style.button} ${style.ctaButton} ${className && style[className]}`} onClick={onClick} type={type} disabled={disabled}>
-				{label}
-			</button>
-		</>
-	);
+  return (
+    <>
+      <button className={`${style.button} ${style.ctaButton} ${className && style[className]}`} onClick={onClick} type={type} disabled={disabled}>
+        {label}
+      </button>
+    </>
+  );
 };
 
 export default CtaButton;
--- src/pages/storic-profile/index.tsx
+++ src/pages/storic-profile/index.tsx
@@ -3,18 +3,17 @@ import withAuth from "@/middleware/withAuth";
 import { getDoc, doc } from "firebase/firestore";
 import { db } from "@/lib/firebaseConfig";
 import { useAuth } from "@/context/AuthContext";
-
 import style from "@/pages/storic-profile/storic-profile.module.scss";
 import Image from "next/image";
-
 import QuizCard from "@/components/Molecules/QuizCard/QuizCard";
 import userAvatar from "@/../public/icons/avatar-user.png";
-
 import Loading from "@/components/Atoms/Loading/Loading";
 import { InterviewSession } from "@/interfaces/interfaces";
 import ResultsList from "@/components/Organism/ResultsList/ResultsList";
 import Link from "next/link";
 import CtaButton from "@/components/Atoms/Buttons/CtaButton";
+import React from "react";
+import AddIcon from "@/../public/icons/add.svg";
 
 const UserProfile = () => {
   const { user } = useAuth();
@@ -76,8 +75,7 @@ const UserProfile = () => {
 
               <header className={style.header}>
                 <div className={style.userInfo}>
-                  <Image src={userAvatar} alt="Profile avatar" width={50} height={50} />
-                  <div>
+                  <div className={style.NameAndMail}>
                     <h4>
                       <span>{user.displayName}</span>
                     </h4>
@@ -85,17 +83,39 @@ const UserProfile = () => {
                       <span>{user.email}</span>
                     </h4>
                   </div>
+                  <Image src={userAvatar} alt="Profile avatar" width={40} height={40} />
                 </div>
-                <h2>Le tue statistiche</h2>
               </header>
 
-              <ul className={style.cardsList}>
-                {interviewSessions.map((interviewSession, index) => (
-                  <li key={index}>
-                    <QuizCard interviewSession={interviewSession} onDelete={handleDeleteSession} setSelectedInterviewSession={setSelectedInterviewSession} />
-                  </li>
-                ))}
-              </ul>
+              <div className={style.headerButtons}>
+                <Link href="./topic-process">
+                  <button>
+                    <Image src={AddIcon} className={style.icon} alt="Time Icon" width={20} height={20} />
+                    Argomento
+                  </button>
+                </Link>
+                <Link href="./interview-process">
+                  <button>
+                    <Image src={AddIcon} className={style.icon} alt="Time Icon" width={20} height={20} />
+                    Colloquio
+                  </button>
+                </Link>
+              </div>
+
+              {/*Lista Card */}
+              <section>
+                <h5 className={style.recentTests}>
+                  Test Recenti
+                  <span>{interviewSessions.length}</span>
+                </h5>
+                <ul className={style.cardsList}>
+                  {interviewSessions.map((interviewSession, index) => (
+                    <li key={index}>
+                      <QuizCard interviewSession={interviewSession} onDelete={handleDeleteSession} setSelectedInterviewSession={setSelectedInterviewSession} />
+                    </li>
+                  ))}
+                </ul>
+              </section>
             </>
           ) : (
             <>
