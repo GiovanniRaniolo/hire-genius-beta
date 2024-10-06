df9629d0d8e3c16a2c2e81544a7e0e6af6d41a15
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-10-06 12:21:04 +0200

    style: fix InterviewRequirements and add new favicon

--- src/components/Organism/InterviewRequirements/InterviewRequirements.tsx
+++ src/components/Organism/InterviewRequirements/InterviewRequirements.tsx
@@ -2,47 +2,47 @@ import CtaButton from "@/components/Atoms/Buttons/CtaButton";
 import TextAreaBox from "@/components/Molecules/TextAreaBox/TextAreaBox";
 import { interviewFormLabels } from "@/constants/menuData";
 
-import style from "./InterviewRequirements.module.scss";
+import style from "../QuestionCard/questionCard.module.scss";
 import { useState } from "react";
 
 interface InterviewRequirementsProps {
-	onSubmit: (interviewRequirements: string) => void;
+  onSubmit: (interviewRequirements: string) => void;
 }
 
 const InterviewRequirements = (props: InterviewRequirementsProps) => {
-	const { onSubmit } = props;
+  const { onSubmit } = props;
 
-	const [requirements, setRequirements] = useState("");
+  const [requirements, setRequirements] = useState("");
 
-	const handleSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-		console.log({ requirements });
-		onSubmit(requirements);
-	};
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ requirements });
+    onSubmit(requirements);
+  };
 
-	return (
-		<main className={style.main}>
-			<div className={style.container}>
-				<header className={style.header}>
-					<h2 className={style.sectionTitle}>{interviewFormLabels.title}</h2>
-				</header>
+  return (
+    <main className={style.main}>
+      <div className={style.container}>
+        <header className={style.header}>
+          <h2 className={style.sectionTitle}>{interviewFormLabels.title}</h2>
+        </header>
 
-				<div>
-					<form onSubmit={handleSubmit}>
-						<TextAreaBox
-							name='interviewDetails'
-							label='Puoi scrivere qui i requisiti specifici per il colloquio che vuoi affrontare.'
-							placeholder='I requisiti sono...'
-							value={requirements}
-							onChange={(e) => setRequirements(e.target.value)}
-							required={false}
-						/>
-						<CtaButton type='submit' label={interviewFormLabels.button} className='ctaB' />
-					</form>
-				</div>
-			</div>
-		</main>
-	);
+        <div>
+          <form onSubmit={handleSubmit}>
+            <TextAreaBox
+              name="interviewDetails"
+              label="Puoi scrivere qui i requisiti specifici per il colloquio che vuoi affrontare."
+              placeholder="I requisiti sono..."
+              value={requirements}
+              onChange={(e) => setRequirements(e.target.value)}
+              required={false}
+            />
+            <CtaButton type="submit" label={interviewFormLabels.button} className="ctaB" />
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default InterviewRequirements;
