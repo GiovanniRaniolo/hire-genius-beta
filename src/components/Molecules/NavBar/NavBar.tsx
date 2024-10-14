e86df4ee1448e61ec845ce4084a4c03ac5352c1e
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-10-14 11:32:30 +0200

    style: add overlay to NavBar component to improve visibility

--- src/components/Molecules/NavBar/NavBar.tsx
+++ src/components/Molecules/NavBar/NavBar.tsx
@@ -17,7 +17,6 @@ const NavBar = () => {
     setIsOpen(!isOpen);
   };
 
-  // Chiude il menu quando l'utente clicca all'esterno del menu
   const handleClickOutside = (event: MouseEvent) => {
     const menuElement = document.getElementById("sidebar-menu");
     if (menuElement && !menuElement.contains(event.target as Node)) {
@@ -26,10 +25,7 @@ const NavBar = () => {
   };
 
   useEffect(() => {
-    // Aggiunge l'evento per chiudere il menu quando si clicca all'esterno
     document.addEventListener("mousedown", handleClickOutside);
-
-    // Rimuove l'evento al momento della pulizia
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
@@ -43,31 +39,35 @@ const NavBar = () => {
       {!isOpen && <ActionButton onClick={toggleMenu} className="round" icon={MenuIcon} />}
 
       {isOpen && (
-        <nav id="sidebar-menu" className={style.sidebar}>
-          <ul className={style.menuList}>
-            {filteredSidebarMenu.map((item) => (
-              <li key={item.label}>
-                <Link href={item.link} className={`${style.menuItem} ${router.pathname === item.link ? style.active : ""}`} onClick={() => setIsOpen(false)}>
-                  <Image src={item.icon} alt={`${item.label} icon`} width={20} height={20} priority={false} />
-                  {item.label}
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <>
+          {/* Overlay */}
+          <div className={style.overlay} onClick={() => setIsOpen(false)}></div>
+          <nav id="sidebar-menu" className={style.sidebar}>
+            <ul className={style.menuList}>
+              {filteredSidebarMenu.map((item) => (
+                <li key={item.label}>
+                  <Link href={item.link} className={`${style.menuItem} ${router.pathname === item.link ? style.active : ""}`} onClick={() => setIsOpen(false)}>
+                    <Image src={item.icon} alt={`${item.label} icon`} width={20} height={20} priority={false} />
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
 
-          <ul className={style.footerList}>
-            {footerMenu.map((item) => (
-              <li key={item.label}>
-                <Link href={item.link} onClick={() => setIsOpen(false)}>
-                  {item.label}
-                </Link>
-              </li>
-            ))}
-            <Link href="/">
-              <p>@2024 HireGenius</p>
-            </Link>
-          </ul>
-        </nav>
+            <ul className={style.footerList}>
+              {footerMenu.map((item) => (
+                <li key={item.label}>
+                  <Link href={item.link} onClick={() => setIsOpen(false)}>
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+              <Link href="/">
+                <p>@2024 HireGenius</p>
+              </Link>
+            </ul>
+          </nav>
+        </>
       )}
 
       <nav className={style.navbar}>
