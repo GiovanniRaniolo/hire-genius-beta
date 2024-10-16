866b2794e4f9323505c3960f3f370553ceead59e
Giovanni Raniolo <gianniraniolo80@gmail.com>
2024-10-16 09:31:18 +0200

    fix: minor fixes on ChooseInterviewer and docs images

--- src/components/Organism/ChooseInterviewer/ChooseInterviewer.tsx
+++ src/components/Organism/ChooseInterviewer/ChooseInterviewer.tsx
@@ -8,46 +8,54 @@ import { ChooseInterviewerProps } from "@/interfaces/interfaces";
 import { profiles } from "@/constants/interviewersProfiles";
 
 const ChooseInterviewer: React.FC<ChooseInterviewerProps> = ({ onInterviewerSelect }) => {
-	const [selectedInterviewer, setSelectedInterviewer] = useState<(typeof profiles)[0] | null>(null);
+  const [selectedInterviewer, setSelectedInterviewer] = useState<(typeof profiles)[0] | null>(null);
 
-	const handleSubmit = (e: FormEvent) => {
-		e.preventDefault();
-		if (selectedInterviewer) {
-			onInterviewerSelect(selectedInterviewer);
-		}
-	};
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (selectedInterviewer) {
+      onInterviewerSelect(selectedInterviewer);
+    }
+  };
 
-	return (
-		<main className={style.main}>
-			<div className={style.container}>
-				<header className={style.header}>
-					<h2 className={style.sectionTitle}>Scegli il tuo Recruiter</h2>
-				</header>
+  return (
+    <main className={style.main}>
+      <div className={style.container}>
+        <header className={style.header}>
+          <h2 className={style.sectionTitle}>Scegli un Esaminatore</h2>
+        </header>
 
-				<div>
-					<form onSubmit={handleSubmit} className={style.interviewerForm}>
-						<div className={style.avatarGrid}>
-							{profiles.map((profile) => (
-								<label key={profile.value} className={`${selectedInterviewer?.value === profile.value ? `${style.label} ${style.selected}` : style.label}`} htmlFor={profile.value}>
-									<div>
-										<div className={style.avatar}>
-											<Image className={style.avatarImg} src={profile.avatarSrc} alt={`Avatar ${profile.name}`} width={1000} height={1000} priority />
-										</div>
-										<h3 className={style.avatarName}>{profile.name}</h3>
-										<p className={style.avatarBio}>{profile.shortBio}</p>
-									</div>
-									<InterviewerBadge label={profile.level} className={profile.className} />
-									<input type='radio' name='interviewer' id={profile.value} value={profile.value} onChange={() => setSelectedInterviewer(profile)} required={false} className={style.radio} />
-								</label>
-							))}
-						</div>
+        <div>
+          <form onSubmit={handleSubmit} className={style.interviewerForm}>
+            <div className={style.avatarGrid}>
+              {profiles.map((profile) => (
+                <label key={profile.value} className={`${selectedInterviewer?.value === profile.value ? `${style.label} ${style.selected}` : style.label}`} htmlFor={profile.value}>
+                  <div>
+                    <div className={style.avatar}>
+                      <Image className={style.avatarImg} src={profile.avatarSrc} alt={`Avatar ${profile.name}`} width={1000} height={1000} priority />
+                    </div>
+                    <h3 className={style.avatarName}>{profile.name}</h3>
+                    <p className={style.avatarBio}>{profile.shortBio}</p>
+                  </div>
+                  <InterviewerBadge label={profile.level} className={profile.className} />
+                  <input
+                    type="radio"
+                    name="interviewer"
+                    id={profile.value}
+                    value={profile.value}
+                    onChange={() => setSelectedInterviewer(profile)}
+                    required={false}
+                    className={style.radio}
+                  />
+                </label>
+              ))}
+            </div>
 
-						<CtaButton label='Cominciamo!' className='ctaA' type='submit' disabled={selectedInterviewer == null} />
-					</form>
-				</div>
-			</div>
-		</main>
-	);
+            <CtaButton label="Cominciamo!" className="ctaA" type="submit" disabled={selectedInterviewer == null} />
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ChooseInterviewer;
